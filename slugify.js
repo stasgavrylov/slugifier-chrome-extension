c6165b0f671a78950f245937887fa64bdd8cb425
Stanislav Gavrylov <stasgavrylov@gmail.com>
2016-11-30 18:46:53 +0200

    initial commit

---
+++ slugify.js
@@ -0,0 +1,25 @@
+function slugifySelection({ selectionText }) {
+  copyToClipboard(slugify(selectionText))
+}
+
+function copyToClipboard(text) {
+  const input = document.createElement('input')
+  input.value = text
+  document.body.appendChild(input)
+  input.select()
+  document.execCommand('Copy')
+  document.body.removeChild(input)
+}
+
+function slugify(text) {
+  return text.toLowerCase().trim()
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/^-+|-+$/g, '')
+}
+
+chrome.contextMenus.create({
+  "title": 'Slugify',
+  "contexts":['selection'],
+  "onclick": slugifySelection,
+})
